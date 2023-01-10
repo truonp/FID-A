e2ea2a922d32f6b464e45cf36f61a7b743f4d805
Peter T <truonp@outlook.com>
2023-01-10 09:56:28 -0500

    Update op_CSIoverlayMRIslice_LCModel.m
    Renamed input/output structure.
    
    Corrected "MRSIStruct.flags" field.
    
    Added "sd_thresh" term which will omit metabolites with %SD above the threshold. Added in term "sd_arr1/2" for storing %SD values for metabolite 1 and 2, respectively.
    
    Added in saving of the lcm csv output filepath. Will bypass loading in file if present.
    
    Fixed up sizing for "metab_arr".
    
    Fixed voxel coordinates.

--- op_CSIoverlayMRIslice_LCModel.m
+++ op_CSIoverlayMRIslice_LCModel.m
@@ -1,14 +1,16 @@
-function csi_av=op_CSIoverlayMRIslice_LCModel(csi_av)
+function MRSIStruct=op_CSIoverlayMRIslice_LCModel(MRSIStruct)
 
-if ~isfield(csi_av.flag,'dcmfound')
+if ~isfield(MRSIStruct.flags,'dcmfound')
     disp('You must run the function <getslice> first, to get the matching dicom image');
     return
 end
 
+sd_thresh=25;%cutoff threshold for %SD
+
 %There will be cases where the CSI FOV is smaller than the image, but focus
 %on case where it's larger for now
 
-dcm_img=dicomread(csi_av.mrsi.dcm_overlay);
+dcm_img=dicomread(MRSIStruct.mrsi.dcm_overlay);
 % [dcm_file dcm_path]=uigetfile('*.IMA');
 % dcm_info=dicominfo([dcm_path filesep dcm_file]);
 % dcm_info=dicominfo('/Users/competer/Documents/data_MR_scans/2022-09-30_JNEA1U_31P_test/JNEA1U_31P_TEST_JNEA1U_31P_TEST/TECHNICAL_JNEA1U_20220930_121308_959000/COR_0004/JNEA1U_31P_TEST.MR.TECHNICAL_JNEA1U.0004.0048.2022.09.30.13.39.21.928538.15245510.IMA');
@@ -18,37 +20,45 @@ dcm_img=dicomread(csi_av.mrsi.dcm_overlay);
 % csi_edges_xy=[csi_pixel_xy(1,1:csi_av.voxelSize.x:end);csi_pixel_xy(2,1:csi_av.voxelSize.y:end)]; %csi grid edges (w.r.t. dicom image mm)
 % csi_pixel_xy=[((-csi_av.fov.x/2):dcm_info.PixelSpacing(1):csi_av.fov.x/2);((-csi_av.fov.y/2):dcm_info.PixelSpacing(2):csi_av.fov.y/2)];
 % csi_edges_xy=[csi_pixel_xy(1,1:csi_av.voxelSize.x:end);csi_pixel_xy(2,1:csi_av.voxelSize.y:cdend)];
-csi_pixel_xy=[((-csi_av.fov.x/2):csi_av.fov.x/2);((-csi_av.fov.y/2):csi_av.fov.y/2)];
-csi_edges_xy=[csi_pixel_xy(1,1:csi_av.voxelSize.x:end);csi_pixel_xy(2,1:csi_av.voxelSize.y:end)];
+csi_pixel_xy=[((-MRSIStruct.fov.x/2):MRSIStruct.fov.x/2);((-MRSIStruct.fov.y/2):MRSIStruct.fov.y/2)];
+csi_edges_xy=[csi_pixel_xy(1,1:MRSIStruct.voxelSize.x:end);csi_pixel_xy(2,1:MRSIStruct.voxelSize.y:end)];
 
 % dcm_fov=[size(dcm_img).*dcm_info.PixelSpacing']; %field of view for the dicom image (mm)
 % dcm_pixel_xy=[((-size(dcm_img,1)/2):size(dcm_img,1)/2).*dcm_info.PixelSpacing(1);((-size(dcm_img,2)/2):size(dcm_img,2)/2).*dcm_info.PixelSpacing(2)]; %pixel locations (in mm) of the image
 dcm_fov=[size(dcm_img)]; %field of view for the dicom image (mm)
 dcm_pixel_xy=[((-size(dcm_img,1)/2):size(dcm_img,1)/2);((-size(dcm_img,2)/2):size(dcm_img,2)/2)]; %pixel locations (in mm) of the image
 
-csi_centre=csi_av.imageOrigin; %(-+)rl;(-+)ap;(-+)fh(is)
+csi_centre=MRSIStruct.imageOrigin; %(-+)rl;(-+)ap;(-+)fh(is)
 
 tmp_csi_shift=csi_pixel_xy+[-csi_centre(2); csi_centre(3)];
 [~,shifted_origin]=min(abs(tmp_csi_shift),[],2);
 
-new_img=zeros(csi_av.fov.x,csi_av.fov.y);
+new_img=zeros(MRSIStruct.fov.x,MRSIStruct.fov.y);
 new_img(shifted_origin(1)-size(dcm_img,1)/2+1:shifted_origin(1)+size(dcm_img,1)/2,shifted_origin(2)-size(dcm_img,2)/2+1:shifted_origin(2)+size(dcm_img,2)/2)=dcm_img;
-imshow(new_img,[]); hold on; xline(1:csi_av.voxelSize.x:csi_av.fov.x,'g'); yline(1:csi_av.voxelSize.x:csi_av.fov.x,'g');
+imshow(new_img,[]); hold on; xline(1:MRSIStruct.voxelSize.x:MRSIStruct.fov.x,'g'); yline(1:MRSIStruct.voxelSize.x:MRSIStruct.fov.x,'g');
 
-% [csv_name csv_path]=uigetfile('*.csv');
-csv_path = '/Users/competer/Documents/data_MR_scans/2022-09-30_JNEA1U_31P_test/FIDA_CSI_output';
-csv_name = 'test.csv';
-metab_data = readtable([csv_path filesep csv_name]);
-csi_av.mrsi.lcm.csv_file=[csv_path filesep csv_name];
-csi_av.mrsi.lcm.met_data=metab_data;
+if ~isfield(MRSIStruct.mrsi,'lcm')
+    [csv_name,csv_path]=uigetfile('*.csv');
+%     csv_path = '/Users/competer/Documents/data_MR_scans/2022-09-30_JNEA1U_31P_test/FIDA_CSI_output';
+%     csv_name = 'test.csv';
+    csv_fullpath=fullfile(csv_path,csv_name);
+    metab_data = readtable(csv_fullpath);
+    MRSIStruct.mrsi.lcm.csv_file=csv_fullpath;
+    MRSIStruct.mrsi.lcm.met_data=metab_data;
+else
+    metab_data=MRSIStruct.mrsi.lcm.met_data;
+end
 
 lcm_names = metab_data.Properties.VariableNames(3:end);
 
 y_range = [min(metab_data.Row),max(metab_data.Row)];
 x_range = [min(metab_data.Col),max(metab_data.Col)];
-metab_arr = zeros(size(metab_data,1)); %metabolite values
+metab_arr = zeros([size(metab_data,1),1]); %metabolite values
 tmp_metab_arr = metab_arr;
 metab_mat = zeros(x_range(2)-x_range(1)+1,y_range(2)-y_range(1)+1); %matrix grid mapping met_arr values to x,y coordinates
+
+sd_arr1=zeros(size(metab_data,1));
+sd_arr2=sd_arr1;
 
 n_metab=0;
 tmp_str='';
@@ -67,7 +77,8 @@ while ~u_valid
         fprintf('Input is out of bounds [1,%d]',n_metab);
     else
         u_valid=1;
-        tmp_metab_arr=metab_data.(lcm_names{metab_num*3-2});
+        sd_arr1=metab_data.(lcm_names{metab_num*3-1});
+        tmp_metab_arr=metab_data.(lcm_names{metab_num*3-2}).*(~any( sd_arr1 >sd_thresh,2));
     end
 end
 
@@ -86,23 +97,23 @@ while ~r_valid
                 u_valid=1;
                 r_valid=1;
                 no_denom=0;
-                tmp_metab_arr=tmp_metab_arr./metab_data.(lcm_names{3*metab_denom-2});
-                metab_arr=tmp_metab_arr(~any( isnan( tmp_metab_arr) | isinf( tmp_metab_arr),2),:);
+                sd_arr2=metab_data.(lcm_names{metab_denom*3-1});
+                tmp_metab_arr=tmp_metab_arr.*(~any( sd_arr2 >sd_thresh,2))./metab_data.(lcm_names{3*metab_denom-2});   
+                tmp_metab_arr(isnan(tmp_metab_arr))=0;
+                metab_arr=tmp_metab_arr.*(~any( isinf( tmp_metab_arr),2));
             end
         end
     elseif strcmpi(include_denom,'n')
         r_valid=1;
-        metab_arr=tmp_metab_arr;
+        metab_arr=tmp_metab_arr.*(~any( isinf( tmp_metab_arr) | isnan( tmp_metab_arr),2));
     else
         sprintf('Invalid response, please input either [Y/N]');
     end
 end
     
-    
-
 for i = 1:length(metab_arr)
-    x = metab_data.Col(i)-x_range(1)+1;
-    y = metab_data.Row(i)-y_range(1)+1;
+    x = x_range(2)-metab_data.Row(i)+1;
+    y = metab_data.Col(i);
     metab_mat(x,y) = metab_arr(i);
 end
 
@@ -113,6 +124,6 @@ cmap = jet(256);
 rgbImage = ind2rgb(uint8(255 * mat2gray(met_img)), cmap);
 ha = imshow(new_img, []);
 hold on;
-hb = imshow(rgbImage);hb.AlphaData = 0.3;
+hb = imshow(rgbImage);hb.AlphaData = 0.5;
 end
 
